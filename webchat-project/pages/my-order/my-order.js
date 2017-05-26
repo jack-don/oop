8cfc90203c1dca036d5298051ad8245475efcb24
DongZhen <67802052@qq.com>
2017-05-26 14:43:56 +0800

    webchat

---
+++ webchat-project/pages/my-order/my-order.js
@@ -0,0 +1,8 @@
+//my-order.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: '我的订单'
+  }
+})
---
+++ webchat-project/pages/personal-center/personal-center.js
@@ -0,0 +1,9 @@
+//personal-center.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: '个人中心',
+    userInfo: {}
+  }
+})
